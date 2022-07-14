cad57619606df323fcfbb2a005e0c5bfc4fa82ff
Akbar Mohammed Kutty <38568371+akbar-yes@users.noreply.github.com>
2022-07-14 23:13:11 +0400

    Update settings.py

--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,10 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR/'static'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
